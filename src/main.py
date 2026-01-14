f84efb6ddb6f00f20753f2f937d4313f89581f61
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-14 03:10:43 -0800

    Add more logic to our calculator

--- src/main.py
+++ src/main.py
@@ -9,24 +9,39 @@ def operation(first_number, next_number, operator):
 
     return calculation[operator]
 
-
-
-
 def main():
+    flow = "restart"
+    result = 0 
     operators = ["+", "-", "*", "/"]
-    
-    first_number = float(input("What's the first number?: "))
+ 
+    while True:
 
-    print(", ".join(operators))   
+        if flow == "restart":
+            first_number = float(input("What's the first number?: "))
 
-    operator = input("Pick an operation: ")
+        print(", ".join(operators))   
 
-    next_number = float(input("What's the next number?: "))
+        operator = input("Pick an operation: ")
 
-    result = operation(first_number, next_number, operator)
+        next_number = float(input("What's the next number?: "))
+        
+        if flow == "restart":
+            result = operation(first_number, next_number, operator)
+        else:
+            result = operation(result, next_number, operator)
 
-    print(f"{first_number} {operator} {next_number} = {result}")
 
+        print(f"{first_number} {operator} {next_number} = {result}")
+        
+        
+        decision = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+
+        if decision == "y":
+            flow = "continue"
+            continue
+
+        print("You choose 'n' or something else, we are starting blank!")
+        flow = "restart"
 
 if __name__ == "__main__":
     main()
